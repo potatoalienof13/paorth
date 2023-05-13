1f26f459db4e57b33909e1a588d6bd84e43499e9
potatoalienof13 <potatoalienof13@gmail.com>
2023-05-13 13:19:44 -0400

    Add an integer parsing function.

--- integers.asm
+++ integers.asm
@@ -22,7 +22,6 @@ putint:
 	; but it still works.
 	neg rdi
 	.loop:
-		; now a positive number, ez to deal with
 		mov rdx, 0
 		mov rax, rdi
 		div qword [base]
@@ -30,7 +29,7 @@ putint:
 		mov dl, [baselookup + rdx]
 		dec rsp
 		mov byte[rsp], dl
-		; exit if done 
+		; exit
 		cmp rax, 0
 		je .fin
 		mov rdi, rax
@@ -46,11 +45,59 @@ putint:
 	mov rsp, rbp
 	pop rbp 
 	ret
-	
-; parses an int - respecting the current base 
+
+; takes a single character, and finds it in the base string
+; O(N), but a smarter solution will make changing the base string
+; harder
+; sets ZF on failure
+chartoint:
+	mov rsi, baselookup
+	mov rax, [base]
+	.loop:
+		lea rcx, [rax + baselookup]
+		cmp rcx, rsi
+		je .err
+		cmp byte[rsi], dil
+		je .end
+		inc rsi
+		jmp .loop 
+	.end:
+		; this sub wont set ZF for 0.
+		sub rsi, baselookup 
+		mov rax, rsi
+		cmp rax, -1 
+		ret
+	.err:
+		; zf conveniently set already
+		ret
+		
+; parses an int - respecting the current base
 ; sets ZF if it does not parse as int
 ; returns the int
 ; takes a null terminated string as argument
 parseint:
-	; todo
+	mov r13, rdi
+	mov r12, 0
+	mov r14, 1
+	cmp byte[r13], '-'
+	jne .loop
+	inc r13
+	mov r14, -1
+	.loop:
+		imul r12, [base]
+		mov dil, byte[r13]
+		; note that this stops empty strings.
+		call chartoint
+		retzf
+		add r12, rax
+		inc r13
+		cmp byte[r13], 0
+		je .end
+		jmp .loop
+	.end:
+		mov rax, r12
+		imul rax, r14
+		test rsp, rsp ; unset zf
+		ret
+; todo
 %endif
