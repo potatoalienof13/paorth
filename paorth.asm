cb14be06cc22d13088a461dda0e4ac1763b67378
potatoalienof13 <potatoalienof13@lolnoemam.dsa>
2023-06-07 14:19:52 -0400

    Paorth interprets when ran.

--- paorth.asm
+++ paorth.asm
@@ -3,49 +3,20 @@
 %include 'readword.asm'
 %include 'exit.asm'
 %include 'integers.asm'
+%include 'interpreter.asm'
 
 global _start
 
 SECTION .data
-message: db 'hello, world'
+message: db 'hello, world', 0
+fail: db 'Failed lol', 0
 
 SECTION .bss
 buffer: resb 64
 
+
 SECTION .text
-
 _start:
-	e:
-		nop ; used because of debugging being stupid
-		mov qword [base], 10
-		mov rdi, 9223372036854775808
-		call putint
-		mov rdi, -13
-		call putint
-		mov rdi, 13
-		call putint
-		mov rdi, 0
-		call putint
-		mov rdi, 1
-		call putint
-		mov rdi, -1
-		call putint
-
-		mov qword [base], 16
-		mov rdi, 9223372036854775808
-		call putint
-		mov rdi, -13
-		call putint
-		mov rdi, 13
-		call putint
-		mov rdi, 0
-		call putint
-		mov rdi, 1
-		call putint
-		mov rdi, -1
-		call putint
-
-
-		call exit
-	;mov rax, _putexiter
-	;jmp docol
+	nop
+	call interpret
+	call exit
