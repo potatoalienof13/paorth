3be03638e7d303abc98da7315a3afb7f4dd4cdac
potatoalienof13 <potatoalienof13@lolnoemam.dsa>
2023-06-07 14:14:25 -0400

    Fix sysv abi non-compliance in find.

--- find.asm
+++ find.asm
@@ -5,15 +5,16 @@
 ; all word definitions need to be defined before here 
 %include 'words.asm'
 %include 'word.asm'
+%include 'streq.asm'
 
 section .data
 latestdefinedword: dq NEXTWORDPTR
 
 section .text
 ; takes a string, and returns a word ptr, or null if it is not found
-
 find:
-	push rbx 
+	push rbx
+	push r13
 	mov rbx, [latestdefinedword]
 	mov r13, rdi
 	.loop:
@@ -26,11 +27,13 @@ find:
 		mov rbx, [rbx + wordtype.next]
 		jmp .loop
 	.wordnotfound:
-		mov rax, 0 
+		mov rax, 0
+		pop r13 
 		pop rbx
 		ret
 	.wordfound:
 		mov rax, rbx 
+		pop r13
 		pop rbx
 		ret 
 ; This will make any word definition after this file fail.
