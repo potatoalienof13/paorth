9e6c42dd75ed983f053455613757f305fd8ebd9d
potatoalienof13 <potatoalienof13@lolnoemam.dsa>
2023-06-07 14:19:10 -0400

    Add interpreter + compilation.

---
+++ compileflag.asm
@@ -0,0 +1,6 @@
+%ifndef compileflag.asm_included
+%define compileflag.asm_included
+section .data 
+compileflag: dq 0
+
+%endif
---
+++ interpreter.asm
@@ -0,0 +1,62 @@
+%ifndef interpreter.asm_included
+%define interpreter.asm_included
+
+
+%include 'readword.asm'
+%include 'integers.asm'
+%include 'find.asm'
+%include 'compileflag.asm'
+
+section .data
+	wordnotfound: db "Word not found!",0
+	
+section .text
+; reads in input, and runs the code
+; r13 controls whether or not its in compiling mode
+interpret:
+	push rbx
+	push r13
+	mov r13, 0
+	.inloop:
+		call readword
+		jz .end
+		mov rdi, wordbuffer
+		call find
+		cmp rax, 0
+		jne .found
+		mov rdi, wordbuffer
+		call parseint
+		mov rbx, rax
+		jz .notfound
+		cmp r13, 0
+		jnz .compileliteral
+		push rbx
+		jmp .inloop
+		.compileliteral:
+			mov rdi, _literal
+			call writeqword 
+			mov rdi, rbx
+			call writeqword
+			jmp .inloop
+		.found:
+			cmp r13, 0
+			je .run
+			test qword [rax + wordtype.flags], FLAG_IMMEDIATE
+			jnz .run
+			lea rdi, [rax + wordtype.definition]
+			call writeqword
+			jmp .inloop
+		.run:
+			add rax, wordtype.definition
+			callword rax
+			mov r13, qword [compileflag] ; word might have changed it
+			jmp .inloop
+		.notfound:
+			mov rdi, wordnotfound
+			call puts
+			call exit
+	.end:
+		pop r13
+		pop rbx
+		ret
+%endif
